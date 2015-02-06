f6e22a9f7e0bda437521055ce740d290bceff9c4
Alfredo Miranda <alfredocdmiranda@gmail.com>
2015-02-05 16:44:34 -0800

    Version 0.2

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from distutils.core import setup, Extension
  
 module1 = Extension('pyiface',
                     define_macros = [('MAJOR_VERSION', '0'),
-                                     ('MINOR_VERSION', '1')], 
+                                     ('MINOR_VERSION', '2')], 
                     sources = ['src/pyiface.c', 'src/iface.c'])
  
 setup(name = 'pyIface',
-      version = '0.1',
+      version = '0.2',
       description = 'It is an interface to get NIC information',
       author = 'Alfredo Miranda',
       author_email = 'alfredocdmiranda@gmail.com',
       url = 'https://github.com/alfredocdmiranda/pyIface',
-      license='MIT',
+      license='GPL v3',
+      long_description='It is an interface to get NIC information',
       ext_modules = [module1])
