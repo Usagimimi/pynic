93d73ee6f1f17589fc694093b2099d1b5e311ed7
Alfredo Miranda <alfredocdmiranda@gmail.com>
2015-04-25 16:24:42 -0700

    Added new example

---
+++ examples/change_ip.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+# Author: Alfredo Miranda
+# E-mail: alfredocdmiranda@gmail.com
+# Date: 04/24/2015
+#
+# This is a sample code to pyIface, which lists NICs on computer and 
+# show information about them.
+
+import pynic
+
+list_interfaces = pynic.get_list_interfaces()
+
+for pos, i in enumerate(list_interfaces):
+    print("{0} - {1}".format(pos, i))
+
+opt = int(input("Choose the interface: "))
+iface = pynic.Iface.get_interface(list_interfaces[opt])
+
+new_addr = input("New IPv4  Address: ")
+print("Last IPv4: {0}".format(iface.inet_addr))
+iface.set_inet_addr(new_addr)
+print("New IPv4: {0}".format(iface.inet_addr))
